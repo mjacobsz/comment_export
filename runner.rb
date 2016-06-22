814cf0cc4e00e4933764952a97d942a90bdbb8ba
Marvin Jacobsz <marvin@tty.nl>
2016-06-22 17:37:21 +0200

    1. new output file logic, 2. keep 'isDeleted' into account

--- runner.rb
+++ runner.rb
@@ -1,6 +1,8 @@
+require 'fileutils'
 require 'nokogiri'
 require 'byebug'
 require 'json'
+require 'date'
 
 class MCThread
 
@@ -24,32 +26,72 @@ end
 
 class Post
 
-  attr_accessor :user_email, :text, :created_at
+  attr_accessor :user_email, :text, :deleted, :created_at
 
-  def initialize(user_email = nil, text = nil, created_at = nil)
+  def initialize(user_email = nil, text = nil, deleted = nil, created_at = nil)
     @user_email = user_email
     @text = text
+    @deleted = deleted
     @created_at = created_at
   end
 
   def to_s
-    "\tMAIL: #{@user_email}\n\tTEXT: #{@text}\nCREATED_AT: #{@created_at}"
+    "\tMAIL: #{@user_email}\n\tTEXT: #{@text}\nDELETED: #{@deleted}\nCREATED_AT: #{@created_at}"
   end
 
 end
 
+module Helper
+
+  def self.create_new_suffix
+    current_max = Dir["results/*"].
+      select { |e| e =~ /out/ }.                             # Get only the files in this dir that match our data output file format
+      tap    { |e| puts e }.
+      map    { |e| e.split(/out|\.json/).last.to_i}.max || 0 # From this set, get the maximum suffix
+
+    new_max = current_max + 1
+
+    raise "\nMAX SUFFIX REACHED: #{new_max}" if new_max > 99
+
+    new_max < 10 ? "0#{new_max}" : new_max.to_s
+  end
+
+end
+
+
+OBLIGATORY_PATH = "/Users/marvin/projects/comment_export"
+
+raise "You should be on this path when running this file: #{OBLIGATORY_PATH}" unless Dir.pwd == OBLIGATORY_PATH
+
+# Open (source) XML file and get namespaces
 handle = File.open(ARGV[0])
 doc =  Nokogiri::XML(handle)
 ns = doc.namespaces
 
+# Logic for (target) output files
+output_filename = ARGV[1] || "results/out.json"
+
+# backup old outfile
+if File.file?("results/out.json")
+  new_filename = "results/out" + Helper::create_new_suffix + ".json"
+  FileUtils.cp "results/out.json", new_filename
+end
+
 threads = {}
+
+deleted_values = 0
+non_deleted_values = 0
+other = 0
 
 doc.xpath('/xmlns:disqus/xmlns:post', ns).each do |nokogiri_post|
   text       = nokogiri_post.xpath('./xmlns:message', ns).text
   email      = nokogiri_post.xpath('./xmlns:author/xmlns:email', ns).text
-  created_at = nokogiri_post.xpath('./xmlns:createdAt', ns).text
+  deleted    = nokogiri_post.xpath('./xmlns:isDeleted', ns).text == "true" ? true : false
+  created_at = DateTime.parse(nokogiri_post.xpath('./xmlns:createdAt', ns).text)
 
-  post = Post.new(email, text, created_at)
+  deleted ? (deleted_values += 1) : (non_deleted_values += 1)
+
+  post = Post.new(email, text, deleted, created_at)
 
   thread_id = nokogiri_post.at_xpath('./xmlns:thread', ns).attributes["id"].value
 
@@ -62,7 +104,10 @@ doc.xpath('/xmlns:disqus/xmlns:post', ns).each do |nokogiri_post|
   threads[thread_id] << post
 end
 
-File.open("bout.json","w") do |f|
+puts "Deleted values: #{deleted_values}"
+puts "Non deleted values: #{non_deleted_values}"
+
+File.open(output_filename,"w") do |f|
   threads.each { |t|
     f.puts(t)
   }
